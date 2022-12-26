20d12b73d6bdd98d74e68c592ef2e0808ca291b3
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2022-12-26 10:56:50 +0100

    cmd $argv

--- ucenjephp.hr/cmd.php
+++ ucenjephp.hr/cmd.php
@@ -1,6 +1,9 @@
 <?php
+//otvoriti cmd, pozicionirati se na putanju ucenjephp.hr direktorija (naredba cd) 
+// izvesti c:\xampp\php\php.exe cmd.php
+echo 'Hello World!' . PHP_EOL;
 
-echo 'Hello World!';
+print_r($argv);
 
 
 //ako se u php-u ne nalazi html tada nam ne treba oznaka za zatvaranje
