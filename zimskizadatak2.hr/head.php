8f721102e338e10490926a228210fa8715340bc8
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2022-12-29 15:07:26 +0100

    index.php i head.php sa include once

--- zimskizadatak2.hr/head.php
+++ zimskizadatak2.hr/head.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zimski zadatak 2</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="js/bootstrap.bundle.js">
-</head>
-<body>
-    <h3>Hello</h3>
-</body>
-</html>
---
+++ zimskizadatak2.hr/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include_once 'head.php';
+    ?>
+</head>
+<body>
+    hello zimski 2
+</body>
+</html>
