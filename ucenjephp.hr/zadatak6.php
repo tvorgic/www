ae553f73019a251289fb5e3b11e33a6005531b48
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-01-22 16:27:21 +0100

    Create zadatak6.php

---
+++ ucenjephp.hr/zadatak6.php
@@ -0,0 +1,10 @@
+<?php
+
+// Program ispisuje zbroj svih primljenih 
+// vrjednosti bez obzira na ključ iz $_GET niza
+
+// ulaz ?a=2&b=2&c=2
+// izlaz: 6
+
+// ulaz ?xxxxx=2&sdf=2&c1=2
+// izlaz: 6
