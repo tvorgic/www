c05140db67abaffae27ec5d5c3a5d09bb07dd48c
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-01-23 17:31:43 +0100

    Update randomzadaci.php

--- ucenjephp.hr/randomzadaci.php
+++ ucenjephp.hr/randomzadaci.php
@@ -16,3 +16,51 @@ for($i = 1; $i <=30; $i++){
      $sum += $i;
 }
 echo $sum;
+
+echo "<hr>";
+
+
+for($i =1; $i<=5;$i++){
+    for($j=1; $j<=$i; $j++){
+        echo "*";
+        if($j<$i){
+            echo " ";
+        }
+    }
+    echo" \n";
+}
+
+echo "<hr>";
+
+
+for($i=1; $i<=5; $i++){
+    for($j=5; $j<=$i; $j++){
+        echo "*";
+    }
+    echo "\n";
+}
+echo "<hr>";
+
+
+for ($row = 1; $row <= 5; $row++)
+{
+    for ($col = 1; $col <= 5; $col++)
+    {
+        echo '*';
+    }
+
+    echo "\n";
+}
+
+echo "<hr>";
+
+
+for ($row = 1; $row <= 5; $row++)
+{
+    for ($col = 1; $col <= $row; $col++)
+    {
+        echo '*';
+    }
+
+    echo "\n";
+}
