6172627674ea01667cc6d295592fa1645b9113d6
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-01-24 17:43:43 +0100

    funkcije

--- ucenjephp.hr/randomzadaci.php
+++ ucenjephp.hr/randomzadaci.php
@@ -64,3 +64,54 @@ for ($row = 1; $row <= 5; $row++)
 
     echo "\n";
 }
+
+echo "<hr>";
+
+/*
+    Homework
+Review Solution
+
+Create an array of your favorites movies, including their respective release dates. Then, write a function that filters your list of movies down to only those that were released in the year 2000 or higher. Display the results in an unordered list.
+*/
+$movies = [
+    [
+        'title' => 'The Lord of the Rings: The Fellowship of the Ring',
+        'director' => 'Peter Jackson',
+        'releaseYear' => 2001
+    ],
+
+    [
+        'title' => 'Blade Runner 2049',
+        'director' => 'Denis Villeneuve',
+        'releaseYear' => 2017
+    ],
+
+    [
+        'title' => 'Pulp Fiction',
+        'director' => 'Quentin Tarantino',
+        'releaseYear' => 1994
+    ]
+
+
+];
+
+foreach ($movies as $movie){
+    echo $movie['title'], '<br>' . $movie['director'], '<br>' . $movie['releaseYear'], '<br>';
+}
+
+
+
+function filterMovie($movies){
+
+    $filteredMovies = [];
+
+    foreach ($movies as $movie){
+      if($movie['releaseYear'] >= 2000){
+        $filteredMovies[] = $movie; 
+      }
+    }
+    return $filteredMovies;
+}
+
+filterMovie($movies);
+
