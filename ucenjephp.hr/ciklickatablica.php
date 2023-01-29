2b62e7cbb0ee7b2743b21b91e582ed0c960dcc9b
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-01-29 23:18:58 +0100

    ideja

--- ucenjephp.hr/ciklickatablica.php
+++ ucenjephp.hr/ciklickatablica.php
@@ -81,8 +81,8 @@ for($i=$rs;$i<=$re;$i--){
 }
 
 echo $matrica[$cs][$i];
-*/
-
++++++++++++++++++++++++++++++++++++++++++++++
+// ovo dolje donekle radi
 $a=5;
 $b=5;
 $matrica=[];
@@ -105,6 +105,35 @@ for($i=5;$i>=1;$i--){
         
     }
      echo '</tr>';
+}
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
+
+$a=5;
+$b=5;
+$matrica=[];
+$rs = 0;
+$re = -1;
+
+$cs =0;
+$ce= -1;
+
+$init = 1;
+
+echo '<table>';
+for($i=5;$i>=0;$i--){
+    $matrica=[];
+    echo '<tr>';
+    for($j=5;$j>=1;$j--){
+    
+        echo '<td style="text-align: right;">' . $i + $j, '</td>';
+        $matrica=[]; //staviti dvije vrijednosti npr pocetna reda? red? stupac? i brojac?
+                    // nakon toga if uvjet i brojac
+        
+    }
+     echo '</tr>';
+     
 }
 
 //barem nekakav napredak. proučiti dalje 
