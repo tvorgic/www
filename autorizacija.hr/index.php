bacea2bb72a15b369079e12b3c92fc0e99f83217
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-02-19 15:55:02 +0100

    session

--- autorizacija.hr/index.php
+++ autorizacija.hr/index.php
@@ -1,16 +1,26 @@
 <?php
-    session_start();
+session_start();
+
+echo '<pre>';
+print_r($_COOKIE);
+echo '</pre>';
+
+$_SESSION['kljuc'] = 'Hello';
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Javno</title>
 </head>
+
 <body>
     Javni dio
 </body>
+
 </html>
