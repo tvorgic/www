113163e9efe5ad2c4630b110cbedea05e0598139
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2023-02-25 11:29:14 +0100

    Update index.php

--- app.hr/index.php
+++ app.hr/index.php
@@ -1,2 +1,19 @@
 <?php
-echo "hello";
+
+
+//ova datoteka će definirati temeljne preduvjete
+//napraviti autoloading
+
+define('BP',__DIR__ . DIRECTORY_SEPARATOR);
+
+define('BP_APP', BP . 'app' . DIRECTORY_SEPARATOR);
+
+//samo za model core i controller
+
+$zaAutoload=[
+  BP_APP . 'controller',
+  BP_APP . 'core',
+  BP_APP . 'model'
+];
+
+//view ne ide u autoload jer su u njem phtml datoteke
